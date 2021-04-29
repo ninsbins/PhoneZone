a0c1e5b03d02f5e1fba57242b33f52b1857b59a7
mbub5900 <mbub5900@uni.sydney.edu.au>
2021-04-29 17:20:22 -0400

    adding enabling/disabling phone listing functionality

--- server/controllers/phonesController.js
+++ server/controllers/phonesController.js
@@ -42,3 +42,41 @@ exports.best_sellers = (req, res, next) => {
             return res.status(500).json(err);
         });
 };
+
+exports.enable_listing = (req, res, next) => {
+    // take away disable from phone
+    let phoneId = req.body.phoneId;
+
+    Phone.findByIdAndUpdate(phoneId, {
+        $unset: { disabled: 1 },
+    })
+        .then((result) => {
+            return res.status(200).json({
+                message: "successfully enabled listing",
+            });
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                message: "unable to enable phone listing",
+            });
+        });
+};
+
+exports.disable_listing = (req, res, next) => {
+    // add disabled field to phone
+    let phoneId = req.body.phoneId;
+
+    Phone.findByIdAndUpdate(phoneId, {
+        disabled: "",
+    })
+        .then((result) => {
+            return res.status(201).json({
+                message: "successfully disabled",
+            });
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                message: "unable to disable phone listing",
+            });
+        });
+};
