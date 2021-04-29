59bb91e76189148ed3fe98c75fca1cd121c9c1a2
mbub5900 <mbub5900@uni.sydney.edu.au>
2021-04-29 16:53:32 -0400

    adding best sellers controller function

--- server/controllers/phonesController.js
+++ server/controllers/phonesController.js
@@ -4,20 +4,41 @@ const Phone = require("../models/phone");
 exports.sold_out_soon = (req, res, next) => {
     // get 5 phones with lowest quantity
     //
-    Phone.find({stock: {$gte: 1}, disabled: {$exists: false}})
-        .sort({stock: 1})
+    Phone.find({ stock: { $gte: 1 }, disabled: { $exists: false } })
+        .sort({ stock: 1 })
         .limit(5)
-        .populate('seller') // get seller information via reference
+        .populate("seller") // get seller information via reference
         .exec()
         .then((result) => {
             res.status(200).json(result);
-        }).catch((err) => {
+        })
+        .catch((err) => {
             console.log(err);
-            res.status(500).json({error: err });
+            res.status(500).json({ error: err });
         });
-                    
 };
 
 exports.best_sellers = (req, res, next) => {
-    // get 5 phones with highest rating
-}
+    // get 5 phones with highest average rating
+    // reviews.count >= 2
+
+    Phone.aggregate([
+        {
+            $match: {
+                "reviews.1": { $exists: true },
+                disabled: { $exists: false },
+            },
+        },
+        { $addFields: { RatingAverage: { $avg: "$reviews.rating" } } },
+    ])
+        .sort({ RatingAverage: -1 })
+        .limit(5)
+        .then((result) => {
+            console.log(result);
+            return res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+};
--- server/routes/phones.js
+++ server/routes/phones.js
@@ -3,11 +3,16 @@ var router = express.Router();
 const mongoose = require("mongoose");
 const PhonesController = require("../controllers/phonesController");
 
-/* GET users listing. */
+/* GET phone listing. */
 router.get("/", function (req, res, next) {
     res.send("respond with a resource");
 });
 
 router.get("/soldoutsoon", PhonesController.sold_out_soon);
+
+/**
+ * GET top rated phones (top 5)
+ */
+router.get("/bestsellers", PhonesController.best_sellers);
 
 module.exports = router;
