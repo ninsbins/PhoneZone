98303c80963e1df55df9704dc64250b0b844c479
Jeremy Gillen <jgil9485@uni.sydney.edu.au>
2021-05-12 01:25:36 +1000

    added multiple tabs to page using router

--- client/src/pages/UserProfilePage.js
+++ client/src/pages/UserProfilePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useAuth } from "../services/useAuth";
 import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 const UserProfile = () => {
     let auth = useAuth();
@@ -14,16 +15,20 @@ const UserProfile = () => {
             .get(`http://localhost:9000/users/${auth.user}`)
             .then((result) => {
                 console.log(result);
-                setUserDetails(result.data.user);
-                setLoading(false);
-                setError(false);
+                if(loading){
+                    setUserDetails(result.data.user);
+                    setLoading(false);
+                    setError(false);
+                }
             })
             .catch((error) => {
                 console.log(error);
-                setLoading(false);
-                setError(true);
+                if(!error){
+                    setLoading(false);
+                    setError(true);
+                }
             });
-    }, []);
+    });
 
     if (loading) {
         return <div>Loading....</div>;
@@ -34,14 +39,61 @@ const UserProfile = () => {
     }
 
     return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/edit">Edit Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/listings">Manage Listings</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/edit">
+                        <Edit userdetails={userDetails} />
+                    </Route>
+                    <Route path="/listings">
+                        <ManageListings userdetails={userDetails} />
+                    </Route>
+                    <Route path="/">
+                        <Profile userdetails={userDetails} />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+function Profile({userdetails,}) {
+    return (
         <div>
             <ul>
-                <li>firstName: {userDetails.firstname}</li>
-                <li>lastName: {userDetails.lastname}</li>
-                <li>email: {userDetails.email}</li>
+                <li>firstname: {userdetails.firstname}</li>
+                <li>lastname: {userdetails.lastname}</li>
+                <li>email: {userdetails.email}</li>
             </ul>
         </div>
     );
-};
+}
+function Edit({userdetails,}) {
+    return (
+        <div>
+        Put a form here
+        </div>
+    );
+}
+function ManageListings({userdetails,}) {
+    return (
+        <div>
+        manage listings here
+        </div>
+    );
+}
 
 export default UserProfile;
